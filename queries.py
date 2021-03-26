6c37ad8333ff2ba2a466a916bdb421e6863d1f4f
Hafidh Khoiri <grosirpc@gmail.com>
2021-03-26 11:07:14 +0700

    Update queries.py

--- queries.py
+++ queries.py
@@ -3,20 +3,12 @@ import config
 
 def getQueryRelationUserTag(userEmail):
     sql = """
-    select TAG.UFK_TAG, relTag.UFK_USER from
-    antares_dnr.dbo.relationUser_TAG relTag
-    inner join antares_dnr.dbo.tblTemplateAssignmentGroup TAG
-    on relTag.TAG_ID = TAG.TAG_ID
-    where relTag.UFK_USER in (
-    select ufk_user
-    from antares.dbo.tblUser where USER_ADDRESS_EMAIL = '{0}')
+    Some queries'{0}')
     """.format(userEmail)
     return sql
 
 
 def geQuerytR2UserGuid(userEmail):
     sql = """
-    select R2USER_GUID
-    from antares.dbo.tblUser
-    where USER_ADDRESS_EMAIL = '{0}'""".format(userEmail)
+    Some queries= '{0}'""".format(userEmail)
     return sql
